afa3be6feb2b06a5c5216da665ee6339023bdde9
Murillo Marques <murillomarques2001@gmail.com>
2024-07-17 10:55:23 -0300

    Creating the password validation regex

--- src/userCreator/validation/UserPasswordRegex.java
+++ src/userCreator/validation/UserPasswordRegex.java
@@ -1,4 +1,20 @@
 package userCreator.validation;
 
 public class UserPasswordRegex {
+    public static void passwordCheck(String password) throws Exception {
+        if (!password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")){
+        throw new Exception("""
+                The password must contain:\
+
+                At least one lowercase letter\
+
+                At least one uppercase letter\
+
+                At least one digit\
+
+                At least one special character\
+
+                Minimum length of 8 characters""");
+        }
+    }
 }
